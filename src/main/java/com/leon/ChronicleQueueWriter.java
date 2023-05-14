532779be2b7ffd8b7a62a74a7718a6ae1f8f3427
LeonAdeoye <leon.adeoye@gmail.com>
2023-05-14 21:55:32 +0800

    added end of stream code

--- src/main/java/com/leon/ChronicleQueueWriter.java
+++ src/main/java/com/leon/ChronicleQueueWriter.java
@@ -1,17 +1,8 @@
 package com.leon;
 
-import net.openhft.chronicle.core.io.IORuntimeException;
 import net.openhft.chronicle.queue.ChronicleQueue;
 import net.openhft.chronicle.queue.ExcerptAppender;
-import net.openhft.chronicle.queue.ExcerptTailer;
-import net.openhft.chronicle.queue.RollCycles;
-import net.openhft.chronicle.wire.ReadMarshallable;
-import net.openhft.chronicle.wire.Wire;
-import net.openhft.chronicle.wire.WireIn;
 import org.apache.log4j.BasicConfigurator;
-
-import java.io.IOException;
-import java.util.TimeZone;
 import java.util.UUID;
 
 public class ChronicleQueueWriter
@@ -35,9 +26,8 @@ public class ChronicleQueueWriter
         }
     }
 
-    public ChronicleQueueWriter() throws IOException
+    public ChronicleQueueWriter()
     {
-        String sample = String.format("CASH_CHECK_REQUEST={\"clientId\": %s, \"instrumentId\":  %d, \"requestType\":  \"CASH_CHECK_REQUEST\"}", UUID.randomUUID(), 1_000_000);
         queue = ChronicleQueue.singleBuilder("C:\\data\\chronicle-queue-input").blockSize(1024 * 1024).build();
         appender = queue.acquireAppender();
     }
